20d7a1535342969f10032476b918c87c801eb6ed
Alex Stum <alexkstum@gmail.com>
2026-02-05 10:46:03 -0600

    sddt_support.py
    - Created new module for common functions: pyErr and arcpyErr
    - updated __init__.py to import these functions

--- sddt/__init__.py
+++ sddt/__init__.py
@@ -5,5 +5,7 @@ Created on Tue May  2 19:00:56 2023
 @author: Alexander.Stum
 """
 
-# import sddt.download
-# __all__ = ["download"]
+from .sddt_support import pyErr
+from .sddt_support import arcpyErr
+
+__all__ = ["pyErr", "arcpyErr"]
