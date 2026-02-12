4949c00f1c8181c3897e4b66d40abf788729ec2f
Alex Stum <alexkstum@gmail.com>
2026-02-12 08:45:47 -0600

    join.py, v 0.2
    - Enabled the joining of tables with differing mukey data types which will
    allow the joining of tables from gpkg's which have numeric mukey field which
    also stands in as the OID field

--- sddt/manage/join.py
+++ sddt/manage/join.py
@@ -11,14 +11,20 @@ Soil Information tool
     @title:  GIS Specialist & Soil Scientist
     @organization: National Soil Survey Center, USDA-NRCS
     @email: alexander.stum@usda.gov
-@modified 02/09/2026
+@modified 02/12/2026
     @by: Alexnder Stum
-@version: 0.1
+@version: 0.2
+
+# --- Updated, v 0.2
+- Enabled the joining of tables with differing mukey data types which will
+allow the joining of tables from gpkg's which have numeric mukey field which
+also stands in as the OID field
 """
-v = '0.1'
+v = '0.2'
 
 import arcpy
 import sys
+import pyarrow as pa
 
 from .. import pyErr
 from .. import arcpyErr
@@ -38,8 +44,8 @@ def main(args) -> bool:
         else:
             tab_n = tab.value
         field_names = [f.name for f in arcpy.ListFields(tab) 
-               if f.type not in ['OID', 'Geometry'] 
-               and not f.name.lower().startswith('shape_')]
+               if f.name == 'mukey' or (f.type not in ['OID', 'Geometry'] 
+               and not f.name.lower().startswith('shape_'))]
         if 'mukey' in field_names:
             muk_f0 = 'mukey'
         elif 'MUKEY' in field_names:
@@ -53,21 +59,28 @@ def main(args) -> bool:
                 arcpy.AddError(f"mukey field not found in {tab_n}")
                 return ''
         
+        
         tab0 = arcpy.da.TableToArrowTable(
             in_table=tab,
             field_names=field_names
         )
-        arcpy.AddMessage('Loaded' + tab_n)
+        key_type = tab0.schema.field(muk_f0).type
+        arcpy.AddMessage('\tLoaded ' + tab_n)
+
+        ex_fields = ['areasymbol', 'musym', 'muname', 'areaname']
         for ti, tab in enumerate(input_tabs[1:]):
             muk_f1 = ''
+            # Get table name, either layer or feature
             if 'Table' in str(type(tab)):
                 tab_n = tab.name
             else:
                 tab_n = tab.value
+
+            # Curate fields
             field_names = [f.name for f in arcpy.ListFields(tab) 
-                if f.type not in ['OID', 'Geometry'] 
-                and not f.name.lower().startswith('shape_')
-                and not f.name.lower() == 'areasymbol']
+                if f.name == 'mukey' or (f.type not in ['OID', 'Geometry'] 
+               and not f.name.lower().startswith('shape_')
+                and f.name.lower() not in ex_fields)]
             if 'mukey' in field_names:
                 muk_f1 = 'mukey'
             elif 'MUKEY' in field_names:
@@ -86,11 +99,19 @@ def main(args) -> bool:
                         f"mukey field not found in {tab_n}"
                     )
                     return ''
+                
             tab1 = arcpy.da.TableToArrowTable(
                 in_table=tab,
                 field_names=field_names,
             )
-            arcpy.AddMessage('Loaded' + tab_n)
+            
+            # Check key data type
+            if key_type != tab1.schema.field(muk_f1).type:
+                schema1 = tab1.schema
+                fi = schema1.get_field_index(muk_f1)
+                schema2 = schema1.set(fi, pa.field(muk_f1, key_type))
+                tab1 = tab1.cast(schema2)
+                
             # rename duplicative field names
             cur_fnames = tab0.column_names
             relab_b = False
@@ -104,6 +125,7 @@ def main(args) -> bool:
                 tab1 = tab1.rename_columns(field_names)
 
             tab0 = tab0.join(tab1, muk_f0, muk_f1, "full outer")
+            arcpy.AddMessage('\tLoaded ' + tab_n)
        
         arcpy.AddMessage(f"Completed: {out_tab.value}")
         # arcpy.AddMessage(out_tab.path)
